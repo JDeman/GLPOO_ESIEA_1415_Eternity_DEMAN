8b3a8547c1c81a5e7c1f90c56aeb5fc2d647b669
OcalMagic <oliviercoffin@yahoo.fr>
2015-05-14 02:22:18 +0200

    Menu avec image de fond et bouton

--- Eternity/src/fenetreVue/Menu.java
+++ Eternity/src/fenetreVue/Menu.java
@@ -18,35 +18,61 @@ public class Menu extends JFrame implements ActionListener {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private JPanel boutton;
-	private JButton jouer;
-	private JButton charger;
-	private JButton quitter;
-
 	  public Menu(){
 	    this.setTitle("Menu");
-	    this.setSize(1000, 800);
+	    this.setSize(500, 300);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setLocationRelativeTo(null);
-	    choix();
-	   
-	    this.setContentPane(new ImgFond());
+	    
+	    this.setContentPane(new Panneau());
 	    this.setVisible(true);
 	  }      
 	 
 	  
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Test1");
+	}
+	
+}
+
+class Panneau extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private JButton jouer;
+	private JButton charger;
+	private JButton quitter;
+	
+	
+	public Panneau(){
+		choix();
+	}
+	
+	public void paintComponent(Graphics g){
+			try {
+		      Image img = ImageIO.read(getClass().getResource("/Images/index.jpg"));
+		      //g.drawImage(img, 0, 0, this);
+		      //Pour une image de fond
+		      g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }                
+	}
+	
 	public void choix(){
-		
-		boutton = new JPanel();
-		boutton.setBounds(0, 0, 300, 200);
 
 		jouer();
 		charger();
 		quitter();
 		
-		boutton.add(jouer);
-		boutton.add(charger);
-		boutton.add(quitter);		
+		this.add(jouer);
+		this.add(charger);
+		this.add(quitter);		
 	}
 
 	public void jouer(){
@@ -72,15 +98,7 @@ public class Menu extends JFrame implements ActionListener {
 		//quitter.addActionListener(new Action_quit());
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("Test1");
-	}
-	
-	
 }
-
 
 class Action_jouer extends Menu implements ActionListener{
 
@@ -125,23 +143,4 @@ class Action_quit extends Menu implements ActionListener{
 		System.out.println("Test4");
 	}
 
-}
-
-class ImgFond extends JPanel{
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	public void paintComponent(Graphics g){
-			try {
-		      Image img = ImageIO.read(getClass().getResource("/Images/p1.jpg"));
-		      //g.drawImage(img, 0, 0, this);
-		      //Pour une image de fond
-		      g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }                
-	} 
 }
