cb96785e5114d33f8475d337c063c2ec938e5c6d
OcalMagic <oliviercoffin@yahoo.fr>
2015-05-15 03:03:51 +0200

    Code "mis au propre" et rajout des commentaires

--- Eternity/src/fenetreVue/FenetrePrincipale.java
+++ Eternity/src/fenetreVue/FenetrePrincipale.java
@@ -14,21 +14,24 @@ import fenetreVue.Menu;
 
 public class FenetrePrincipale extends JFrame {
 
-		/**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public static JFrame fenetrePrincipale = new JFrame();
-		private JPanel espJeu;
-		private JMenuBar barMenu;
-		private JMenu option;
-		private JMenuItem restart, quitter;
-		private gameArea terrainJeu;
-		private stockArea terrainStock;
 	
+	//Variable utilisable dans tous le code
+	public static JFrame fenetrePrincipale = new JFrame();
+	
+	private JPanel espJeu;
+	private JMenuBar barMenu;
+	private JMenu option;
+	private JMenuItem restart, quitter;
+	private gameArea terrainJeu;
+	private stockArea terrainStock;
+
 	// Creation de la fenetre principale
 	public FenetrePrincipale() {
-		
+
 		fenetrePrincipale.setTitle("Jeu Eternity - Menu principale");
 		fenetrePrincipale.setSize(1200, 700);
 		fenetrePrincipale.setLocationRelativeTo(null);
@@ -38,62 +41,55 @@ public class FenetrePrincipale extends JFrame {
 		fenetrePrincipale.setContentPane(espJeu);
 		fenetrePrincipale.setJMenuBar(barMenu);
 		fenetrePrincipale.setVisible(true);
-		
 	}
-		
-	
+
 	public void fenetreJeu() {
-		
+
 		buildgameArea();
 		buildstockArea();
-		
+
 		espJeu = new JPanel();
 		espJeu.setBackground(Color.LIGHT_GRAY);
 		espJeu.setBounds(0, 0, 1200, 600);
 		espJeu.setLayout(new BorderLayout());
-		
+
 		espJeu.add(terrainJeu, BorderLayout.EAST);
 		espJeu.add(terrainStock, BorderLayout.WEST);
-		
 	}
-	
+
 	// Affichage du terrain de jeu
 	public void buildgameArea() {
-	
+
 		terrainJeu = new gameArea();
-		
 	}
-	
+
 	// Affichage du terrain de stockage des cartes
 	public void buildstockArea() {
-		
+
 		terrainStock = new stockArea();
-		
 	}
 
 
 	// Creation du menu
 	public void menu() {
-			
+
 		barMenu = new JMenuBar();
-		
-		option = new JMenu("Option");
+
+		option = new JMenu("Options");
 		barMenu.add(option);
-			
+
 		restart = new JMenuItem("Revenir au menu");
 		restart.addActionListener(new Back_menu());
 		option.add(restart);
-		
 
-			
 		quitter = new JMenuItem("Quitter");
 		quitter.addActionListener(new Quitter());
 		option.add(quitter);
-		
+
 	}
-	
+
 	public void MouseClicked (ActionEvent e) {
-		
+
 	}
 
 	class Back_menu implements ActionListener{
@@ -101,23 +97,21 @@ public class FenetrePrincipale extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			//Créer un nouveau Menu
 			new Menu();
-			fenetrePrincipale.dispose();	//enlève la fenetre principale
+			//Masque la fenetre de jeu
+			fenetrePrincipale.dispose();
 			//menu.setVisible(true);
 		}
 	}
-	
+
 	class Quitter implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			//Quitte le programme
 			System.exit(0);
 		}
-		
 	}
-
 }
-
-
-
--- Eternity/src/fenetreVue/Menu.java
+++ Eternity/src/fenetreVue/Menu.java
@@ -17,22 +17,24 @@ public class Menu extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+			
+	//Variable utilisable dans tous le code
 	public static JFrame menu = new JFrame();
-	
-	  public Menu(){
-	    menu.setTitle("Menu");	
-	    menu.setSize(500, 300);
-	    menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    menu.setLocationRelativeTo(null);
-	    
-	    menu.setContentPane(new Panneau());
-	    menu.setVisible(true);
-	  }      
-	
+
+	//création du Menu
+	public Menu(){
+		menu.setTitle("Menu");	
+		menu.setSize(500, 300);
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menu.setLocationRelativeTo(null);
+
+		menu.setContentPane(new Panneau());
+		menu.setVisible(true);
+	}      
 }
 
 class Panneau extends JPanel{
-	
+
 	/**
 	 * 
 	 */
@@ -41,29 +43,27 @@ class Panneau extends JPanel{
 	private JButton jouer;
 	private JButton charger;
 	private JButton quitter;
-	
-	
+
 	public Panneau(){
 		choix();
 	}
-	
+
+	//Image de fond
 	public void paintComponent(Graphics g){
-			try {
-		      Image img = ImageIO.read(getClass().getResource("/Images/index.jpg"));
-		      //g.drawImage(img, 0, 0, this);
-		      //Pour une image de fond
-		      g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }                
+		try {
+			Image img = ImageIO.read(getClass().getResource("/Images/index.jpg"));
+			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}                
 	}
-	
+
 	public void choix(){
 
 		jouer();
 		charger();
 		quitter();
-		
+
 		this.add(jouer);
 		this.add(charger);
 		this.add(quitter);		
@@ -72,28 +72,34 @@ class Panneau extends JPanel{
 	public void jouer(){
 		jouer = new JButton("Jouer");
 		jouer.setPreferredSize(new Dimension(100, 50));
-	/*	try{
+		/*	
+		 	//Image à la place d'un bouton moche
+		 	try{
 			Image img = ImageIO.read(getClass().getResource("/Images/p1.jpg"));
 			jouer.setIcon(new ImageIcon(img));
-		}
-		catch (IOException ex) {}*/
+			}catch (IOException ex) {}
+		*/
 		jouer.addActionListener(new Action_jouer());
 	}
+	
 	class Action_jouer implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			//Ouvre la fentre de jeu
 			new FenetrePrincipale();
+			//Masque le Menu
 			Menu.menu.dispose();
 		}
 	}
-	
+
 	public void charger(){
 		charger = new JButton("Charger");
 		charger.setPreferredSize(new Dimension(100, 50));
 		charger.addActionListener(new Action_load());
 	}
+	
 	class Action_load implements ActionListener{
 
 		@Override
@@ -101,24 +107,21 @@ class Panneau extends JPanel{
 			// TODO Auto-generated method stub
 			//System.out.println("Test3");
 		}
-
 	}
-	
+
 	public void quitter(){
 		quitter = new JButton("Quitter");
 		quitter.setPreferredSize(new Dimension(100, 50));
 		quitter.addActionListener(new Action_quit());
 	}
+	
 	class Action_quit implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			//Quitte le programme
 			System.exit(0);
 		}
 	}
 }
-
-
-
-
